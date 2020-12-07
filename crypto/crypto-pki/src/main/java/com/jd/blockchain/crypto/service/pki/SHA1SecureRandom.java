2c2632d78c9805a77927e5cf522f34f1fe15d944
huanghaiquan <huanghaiquan@jd.com>
2020-12-07 12:50:18 +0800

    optimized the HashBaseSecureRandom that output the same random bytes sequence with the same total size, regardless with how many times of invoking.

--- crypto/crypto-pki/src/main/java/com/jd/blockchain/crypto/service/pki/SHA1SecureRandom.java
+++ crypto/crypto-pki/src/main/java/com/jd/blockchain/crypto/service/pki/SHA1SecureRandom.java
@@ -10,20 +10,25 @@ import com.jd.blockchain.crypto.base.HashBaseSecureRandom;
  * @author huanghaiquan
  *
  */
-class SHA1SecureRandom extends HashBaseSecureRandom {
+public class SHA1SecureRandom extends HashBaseSecureRandom {
 
 	private static final long serialVersionUID = 5750528439654395936L;
+	
+	public static final int DIGEST_LENGTH = 20;
 	
 	public SHA1SecureRandom(byte[] seed) {
 		super(seed);
 	}
-
+	
 	@Override
-	protected byte[] hash(byte[] bytes) {
+	protected int getHashSize() {
+		return DIGEST_LENGTH;
+	}
+	
+	@Override
+	protected void hash(byte[] bytes, byte[] output, int offset) {
 		SHA1Digest sha1Digest = new SHA1Digest();
-		byte[] result = new byte[sha1Digest.getDigestSize()];
 		sha1Digest.update(bytes, 0, bytes.length);
-		sha1Digest.doFinal(result, 0);
-		return result;
+		sha1Digest.doFinal(output, offset);
 	}
 }
