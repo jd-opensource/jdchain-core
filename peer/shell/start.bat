509b6c8e29a0013f9d9e1359a6af29cb18c5905e
wangzhiyong69 <wangzhiyong69@jd.com>
2021-11-30 10:48:04 +0800

    删除多余提交代码

--- peer/shell/start.bat
+++ peer/shell/start.bat
@@ -1,1 +1,1 @@
-java -jar peer-0.0.1-SNAPSHOT.jar --spring.configProperty.location=file:configProperty/application.properties
+java -jar peer-0.0.1-SNAPSHOT.jar --spring.config.location=file:config/application.properties
